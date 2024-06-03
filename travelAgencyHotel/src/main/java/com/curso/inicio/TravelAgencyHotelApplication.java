1f967710e90e28c80b3fe5c439c28a807c6582a5
Shaekhrom <shaekhrom@gmail.com>
2024-06-03 15:30:21 +0200

    si

---
+++ travelAgencyHotel/src/main/java/com/curso/inicio/TravelAgencyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.curso.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyHotelApplication.class, args);
+	}
+
+}
