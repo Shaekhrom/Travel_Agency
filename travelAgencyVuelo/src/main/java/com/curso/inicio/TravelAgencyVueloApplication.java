8e998841b9ac85cb113acff8ade21cc3acb06ab0
Shaekhrom <shaekhrom@gmail.com>
2024-06-03 16:54:37 +0200

    Vuelo 1

---
+++ travelAgencyVuelo/src/main/java/com/curso/inicio/TravelAgencyVueloApplication.java
@@ -0,0 +1,13 @@
+package com.curso.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyVueloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyVueloApplication.class, args);
+	}
+
+}
